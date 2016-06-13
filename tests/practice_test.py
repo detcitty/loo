dc2b10c960d876482c8a02267ff096b579786589
Devin Etcitty <devin.etcitty@gmail.com>
2016-06-13 16:23:39 -0400

    Built logistic.stan model w/ python

--- tests/practice_test.py
+++ tests/practice_test.py
@@ -1,11 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+%matplotlib inline
+
+
 import pandas as pd
 import numpy as np
+import scipy as sp
+import patsy
+import pystan
+
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+
+
+R = ro.r
+
+import statsmodels.api as sm
+
+import rpy2.robjects as ro
+#import pandas.rpy.common as com
+
 import matplotlib.pyplot as platform
 pd.set_option('max_columns', 50)
+
+from decimal import *
+
+print getcontext()
+
 
 url = "http://stat.columbia.edu/~gelman/arm/examples/arsenic/wells.dat"
 wells = pd.read_table(url, sep='\s+', header=0, index_col=0)
 print wells.head()
+wells["dist100"] = wells['dist']/100
+
+y = wells['switch']
+wellsNp = wells.as_matrix()
+y_mod = wellsNp[:, 0]
+
+print y_mod
+
+#print y.shape()
+y_hat = y.transpose()
+
+#print y_hat.shape()
+
+#y_tilda = wells.as_matrix(columns=[2])
+#print "This is y_tilda", y_tilda
+y_mean = np.mean(y_mod)
+#print y_mean
+
+s = sp.special.logit(y_mean)
+print s
+#logit = sm.Logit(np.array(np.mean(y)), wells)
+#fit = logit.fit()
+logit = np.log(y_mean / (1 - y_mean))
+#print fit.results()
+#print log
+f = "~ 0 + dist + arsenic"
+mat = patsy.dmatrix(f, wells, return_type='dataframe')
+nrow, ncol = mat.shape
+#print nrow, ncol
+
+
+#print mat.columns
+#print np.array(mat['dist'])
+#print np.array(mat['arsenic'])
+
+#print logit.results()
+data_loo = {'N': nrow , 'P': ncol, 'y': y, 'x': mat, 'a': s }
+print data['a']
