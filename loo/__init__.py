15324ee694c223938c987be7264d519484f5ef81
Devin Etcitty <devin.etcitty@gmail.com>
2016-06-21 14:38:44 -0400

    Modifed __init__.py in /loo, fixed tests

--- loo/__init__.py
+++ loo/__init__.py
@@ -3,3 +3,12 @@
 __author__ = 'Devin Etcitty'
 __email__ = 'dce2108@columbia.edu'
 __version__ = '0.1.0'
+
+
+__all__ = ['gpdfitnew', 'gpinv', 'psisloo', 'psislw', 'sumlogs']
+
+from loo.gpdfitnew import gpdfitnew
+from loo.gpinv import gpinv
+from loo.psisloo import psisloo
+from loo.psislw import psislw
+from loo.sumlogs import sumlogs
